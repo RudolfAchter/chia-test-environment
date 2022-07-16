8346c680056c8b28e83260c9d92a656e456b4c4d
Rudolf Achter <rudolf.achter@gmail.com>
2022-07-16 21:49:29 +0200

    fix prepare lxd guest

--- src/lxd/prepare_lxd_guest.sh
+++ src/lxd/prepare_lxd_guest.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
 NODE="chia-full01"
 SSHPUBKEY=`cat ~/.ssh/id_rsa.pub`
-lxc exec $NODE – /bin/bash -c "apt install -y openssh-server"
-lxc exec $NODE – /bin/bash -c "useradd -m -G sudo -s /bin/bash ansible"
-lxc exec $NODE – /bin/bash -c "mkdir /home/ansible/.ssh"
-lxc exec $NODE – /bin/bash -c "echo $SSHPUBKEY > /home/ansible/.ssh/authorized_keys"
-lxc exec $NODE – /bin/bash -c "chown ansible:ansible /home/ansible/.ssh/authorized_keys"
-lxc exec $NODE – /bin/bash -c "chmod 600 /home/ansible/.ssh/authorized_keys"
+sudo lxc exec $NODE -- /bin/bash -c 'apt install -y openssh-server'
+sudo lxc exec $NODE -- /bin/bash -c 'useradd -m -G sudo -s /bin/bash ansible'
+sudo lxc exec $NODE -- /bin/bash -c 'echo ansible:chiatest | chpasswd'
+sudo lxc exec $NODE -- /bin/bash -c 'mkdir /home/ansible/.ssh'
+sudo lxc exec $NODE -- /bin/bash -c "echo $SSHPUBKEY > /home/ansible/.ssh/authorized_keys"
+sudo lxc exec $NODE -- /bin/bash -c 'chown ansible:ansible /home/ansible/.ssh'
+sudo lxc exec $NODE -- /bin/bash -c 'chown ansible:ansible /home/ansible/.ssh/authorized_keys'
+sudo lxc exec $NODE -- /bin/bash -c 'chmod 600 /home/ansible/.ssh/authorized_keys'
 
